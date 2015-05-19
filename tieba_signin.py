87d5ae31520ee710d7e5d056395c94fd21d173c3
Derek52722 <xu.g@mail.scut.edu.cn>
2015-05-19 11:59:09 +0800

    change file name

--- tieba_signin.py
+++ tieba_signin.py
@@ -15,6 +15,7 @@
 from basiccrawler import BasicCrawler
 import urllib2, re, urllib
 import hashlib
+import json
 
 class TiebaSignin(BasicCrawler):
     # Initial Method
@@ -74,10 +75,8 @@ class TiebaSignin(BasicCrawler):
 
 
 
-def sign_in():
+def sign_in(my_cookie, BDUSS):
     tieba_like_url = "http://tieba.baidu.com/f/like/mylike"
-    my_cookie = "BAIDUID=E4F26116B6C64B016B1CF3C9F4F6CB84:FG=1; BIDUPSID=E4F26116B6C64B016B1CF3C9F4F6CB84; BAIDU_WISE_UID=wapp_1430558531086_670; BDRCVFR[feWj1Vr5u3D]=I67x6TjHwwYf0; qh[360]=1; BDRCVFR[35mUMIIWOJm]=mk3SLVN4HKm; H_PS_PSSID=13371_1431_13464_13074_12868_13321_12692_13691_10562_12722_13761_13780_11684_13741_13086_8498; Hm_lvt_90056b3f84f90da57dc0f40150f005d5=1430558563,1430578513; Hm_lpvt_90056b3f84f90da57dc0f40150f005d5=1430578513; HOSUPPORT=1; HISTORY=963dd48ead497232508fcf577f27adc2495d1174889b749cb929c371e0919ef3c471b90dee686c1c9621f4a12933ffac9e0555f17a; BDUSS=XROZWIwSjhCa2VaMlNLYkJnUVdCMGxuNUZWTThoVkM3Zy1wfjBmeTJwcFljbXhWQVFBQUFBJCQAAAAAAAAAAAEAAAA~Tl0D2qTN9dau6eQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFjlRFVY5URVV; PTOKEN=f707e39127b69b344919addb53cf30f0; STOKEN=2364cf6ea5bffad8d36ae905c6f730f49dd27410f645f830845cf91a867e8426; SAVEUSERID=69921eecb7db713d7001fb6bb2d16079; USERNAMETYPE=1; UBI=fi_PncwhpxZ%7ETasTIT-0QyTN%7Emvv3XY1mk7tSnHje34fLlRG7t0sBkQUDfiWDOFPzInkkvu%7E9LmWFw7YTBbrxUA5pXzgnIosQUV8X7Qsp6wJOSfD04H1u5Fo6Gpobi6g-J4HzDTmfLyviuyM7rPmudFuSTQNB-DAY4YQhoMgQLqyISSj4XS3cwGnxAihPa8dcloKHiInLyCDtzMuWn%7E1DY0LL71V60JSAM7dw__"
-    BDUSS = 'XROZWIwSjhCa2VaMlNLYkJnUVdCMGxuNUZWTThoVkM3Zy1wfjBmeTJwcFljbXhWQVFBQUFBJCQAAAAAAAAAAAEAAAA~Tl0D2qTN9dau6eQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFjlRFVY5URVV'
     login_headers = {'Cookie' : my_cookie, 'User-Agent': 'Mozilla/5.0 (SymbianOS/9.3; Series60/3.2 NokiaE72-1/021.021; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/525 (KHTML, like Gecko) Version/3.0 BrowserNG/7.1.16352'}
 
     tb = TiebaSignin()
@@ -112,5 +111,7 @@ def sign_in():
 
 
 if __name__ == "__main__":
-    message = sign_in()
+    with open("tieba.json", "r") as f:
+        my_info = json.load(f)
+    message = sign_in(my_info['my_cookie'], my_info['BDUSS'])
     print message
