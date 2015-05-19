3670a20eaec14dca6356b783c2e9eff93ca660fd
Derek52722 <xu.g@mail.scut.edu.cn>
2015-05-19 11:55:41 +0800

    remove personal info

--- xiami_signin.py
+++ xiami_signin.py
@@ -9,6 +9,7 @@
 # Revision      :   None
 
 import urllib2, re, urllib
+import json
 from basiccrawler import BasicCrawler
 
 class XiamiLogin(BasicCrawler):
@@ -33,10 +34,10 @@ class XiamiLogin(BasicCrawler):
 
         for item in item_list:
             if cmp('冥王之殇：'.decode('GBK'), item.decode('utf-8')) == 0:
-                print 'Loging Success!'
+                print '\nLoging Success!\n'
                 login_success = True
             else:
-                print 'Login Fail!'
+                print '\nLogin Fail!\n'
                 login_success = False
 
         return login_success,content
@@ -48,42 +49,48 @@ class XiamiLogin(BasicCrawler):
         r_exp = r'<div\s*class="idh"><a\s*class="check_in"\shref="(.*?)">(.*?)</a></div>'
         item_list = self.extract_content(index_html, r_exp)
         content = ''
-        if len(item_list) != 0 and cmp('每日签到'.decode('GBK'), item_list[1].decode('utf-8')) == 0:
-            check_in_url = 'http://www.xiami.com' + item_list[0]
-            # print check_in_url
+        for item in item_list:
+            if len(item_list) != 0 and cmp('每日签到'.decode('GBK'), item[1].decode('utf-8')) == 0:
+                check_in_url = 'http://www.xiami.com' + item[0]
+                # print check_in_url
                 
-            check_in_headers = { 'Referer'   : 'http://www.xiami.com/web'}
-            self.set_args(url=check_in_url, headers=check_in_headers)
-            content, cookie = self.get_page_and_cookie()
-            # Check if sign in succeed
-            r_exp = r'<div\s*class="idh">已连续签到(\d+)天</div>'
-            login_days = self.extract_content(content, r_exp)
-            if item_list:
-                print 'You have checked in ' + login_days + ' days!'
-            else:
-                print 'Sign in fail!'
-        else:
-            print 'It\'s not time to sign in again!'
+                check_in_headers = { 'Referer'   : 'http://www.xiami.com/web'}
+                self.set_args(url=check_in_url, headers=check_in_headers)
+                content, cookie = self.get_page_and_cookie()
+                # Check if sign in succeed
+                r_exp = r'<div\s*class="idh">(.*?)</div>'
+                login_days = self.extract_content(content, r_exp)
+                if login_days:
+                    print login_days[1].decode('utf-8') + '\n'
+                else:
+                    print 'Sign in fail!\n'
+
+                return
+        print 'It\'s not time to sign in again!\n'
+        return
 
 
 # Execute
-login_url = 'http://www.xiami.com/web/login'
-login_headers = { 'Referer'   : 'http://www.xiami.com/web/login'
-        } 
-username = '373850089@qq.com'
-pw = '220991029'
-data_dict = {'email'        :   username,
-             'password'     :   pw,
-             'LoginButton'  :   '%E7%99%BB%E5%BD%95'
-             }
-login_data= urllib.urlencode(data_dict)
-login_data = login_data.encode('utf-8')
-xmLog = XiamiLogin(login_url, headers=login_headers, data=login_data)
+def execute(username, pw):
+    login_url = 'http://www.xiami.com/web/login'
+    login_headers = { 'Referer'   : 'http://www.xiami.com/web/login'
+            } 
+    data_dict = {'email'        :   username,
+                 'password'     :   pw,
+                 'LoginButton'  :   '%E7%99%BB%E5%BD%95'
+                 }
+    login_data= urllib.urlencode(data_dict)
+    login_data = login_data.encode('utf-8')
+    xmLog = XiamiLogin(login_url, headers=login_headers, data=login_data)
 
-# Login
-login_success, index_html = xmLog.login_and_goto_index()
+    # Login
+    login_success, index_html = xmLog.login_and_goto_index()
 
-# Login Success, and sign in!
-if login_success:
-    xmLog.sign_in(index_html)
+    # Login Success, and sign in!
+    if login_success:
+        xmLog.sign_in(index_html)
 
+if __name__ == "__main__":
+    with open("xiami.json", "r") as f:
+        my_info = json.load(f)
+    execute(my_info['username'], my_info['pw'])
